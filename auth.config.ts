b49307d18d8c61e4310fabe73df5c932078c5fd4
Yan Turnt <bx6xbchosen1@gmail.com>
2025-02-07 14:58:22 +0500

    chapter 15

---
+++ auth.config.ts
@@ -0,0 +1,34 @@
+import type { NextAuthConfig } from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
+import { z } from 'zod'
+
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      console.log('auth', auth)
+
+      const isLoggedIn = !!auth?.user
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard')
+      if (isOnDashboard) {
+        // if (isLoggedIn) return true
+        // return false // Redirect unauthenticated users to login page
+        return isLoggedIn
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl))
+      }
+      return true
+    },
+  },
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials)
+      },
+    }),
+  ], // Add providers with an empty array for now
+} satisfies NextAuthConfig
